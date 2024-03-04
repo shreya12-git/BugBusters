cf7088d7cd6b92fdf21b26c8221203f6de5744e9
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-03-04 22:53:09 +0530

    added to skillsPage.js

--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -1,37 +1,41 @@
 import React from "react";
-import Nav from "../components/Navbar";
+import Nav1 from "../components/Navbar2";
+import SkillCard from "../components/SkillCard";
 const SkillsPage=()=>{
     return(
         <div>
-            <Nav></Nav>
-            <div>
-                <h1  style={{fontSize:"40px", fontWeight:"bold",display:"flex", justifyContent:"center" }}>Skills</h1>
-                <br></br>
-                <div>
-                <h2 style={{fontSize:"30px", fontWeight:"bold",display:"flex", justifyContent:"center" }}>Approved Skills</h2>
-                <br></br>
-                <div style={{display:"flex",fontSize:"30px", gap:"8px", marginLeft:"30%", fontWeight:"bold"}}>
-                    <div>REACT</div>
-                    <div>90%</div>
-                </div>
-                <div style={{display:"flex",fontSize:"30px", gap:"8px", marginLeft:"30%", fontWeight:"bold"}}>
-                    <div>EXPRESS</div>
-                    <div>70%</div>
-                </div>
-                </div>
+            <Nav1></Nav1>
 
-                <h2 style={{fontSize:"30px", fontWeight:"bold",display:"flex", justifyContent:"center" }}>Unapproved Skills</h2>
-                <div style={{display:"flex",fontSize:"30px", gap:"8px", marginLeft:"30%", fontWeight:"bold"}}>
-                    <div>MACHINE LEARNING</div>
-                    <button >Take Test </button>
-                </div>
-                <div style={{display:"flex",fontSize:"30px", gap:"8px", marginLeft:"30%", fontWeight:"bold"}}>
-                    <div>EXPRESS</div>
-                    <div>70%</div>
-                </div>
+         <div className="mt-8 flex items-center justify-center font-bold text-xl">Approved Skills</div>   
+           
+            <div className="mt-9 w-full flex flex-col items-center justify-center px-8">
+      
+      <SkillCard 
+      skill={"HTML5"} 
+      percentage={"95%"} 
+      color={"#FF3F3F"} 
+      move={true}/>
+      <SkillCard skill={"CSS 3"} percentage={"80%"} color={"#008FFF"}/>
+      <SkillCard skill={"Javascript"} percentage={"60%"} color={"#FFB900"} move={true}/>
+      <SkillCard skill={"ReactJs"} percentage={"50%"} color={"#00FFF3"}/>
+      <SkillCard skill={"NodeJs"} percentage={"30%"} color={"#14DB00"} move={true}/>
+    </div>
 
-            </div>
+    <div className="mt-8 flex items-center justify-center font-bold text-xl"> Unapproved SKills</div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full ">
+    <div className="mt-9 w-full flex flex-col items-center justify-center px-8">
+    <SkillCard skill={"C++"} percentage={"0%"} color={"#008FFF"}/>
+    <SkillCard skill={"Java"} percentage={"0%"} color={"#008FFF"}/>
+
+
+    </div>
+    <div className="mt-9 w-full flex flex-col items-start justify-center px-8 gap-12">
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Take test</button>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Take test</button>
+    </div>
         </div>
+        </div>
+        
     );
 }
 export default SkillsPage;
