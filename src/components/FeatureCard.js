da720b5f011e7451c44280e11cef63323ab7ef1e
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-03-04 18:37:38 +0530

    changes
    chnges

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -3,12 +3,12 @@ import './FeatureCard.css';
 
 const cards = [
   { title: 'Preventive Healthcare', copy: 'Ayurveda emphasizes preventive measures to maintain health and prevent diseases. This includes adhering to seasonal routines (Ritucharya), detoxification processes (Panchakarma), and dietary guidelines tailored to individual constitutions (Prakriti)' ,buttontext:'More'},
-  { title: 'Stress Management', copy: 'Ayurveda places significant importance on mental health. Techniques such as meditation, yoga, and specific herbs are recommended to manage stress, anxiety, and promote mental clarity' },
-  { title: 'Digestive Health', copy: 'Ayurveda considers digestion a crucial factor in overall health. It provides dietary recommendations and herbal remedies to improve digestion, address issues such as indigestion, bloating, and optimize nutrient absorption' },
-  { title: 'Chronic Disease ', copy: 'Ayurveda is often used as a complementary approach in managing chronic diseases such as diabetes, arthritis, and cardiovascular conditions. It focuses on lifestyle modifications, diet, and herbal remedies to support conventional medical treatments'}
+  { title: 'Stress Management', copy: 'Ayurveda places significant importance on mental health. Techniques such as meditation, yoga, and specific herbs are recommended to manage stress, anxiety, and promote mental clarity',buttontext:'More' },
+  { title: 'Digestive Health', copy: 'Ayurveda considers digestion a crucial factor in overall health. It provides dietary recommendations and herbal remedies to improve digestion, address issues such as indigestion, bloating, and optimize nutrient absorption',buttontext:'More' },
+  { title: 'Chronic Disease ', copy: 'Ayurveda is often used as a complementary approach in managing chronic diseases such as diabetes, arthritis, and cardiovascular conditions. It focuses on lifestyle modifications, diet, and herbal remedies to support conventional medical treatments',buttontext:'More'}
 ];
 
-const Card = ({ title, copy }) => {
+const Card = ({ title, copy,buttontext }) => {
   return (
     <div className="card">
       <div className="content">
