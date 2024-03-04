096df85237bace506c361faa00ab0996ebeb1d42
shreya12-git <123259437+shreya12-git@users.noreply.github.com>
2024-03-04 18:36:35 +0530

    CHANGES
    CHANGES

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../components/Navbar';
 import Example from '../components/SlideSow';
+import FeaturesPart from '../components/FeaturesPart';
 const HomePage=()=>{
     return(
 
         <div>
         <div><Nav></Nav></div>
         <div><Example></Example></div>
+        <div><FeaturesPart></FeaturesPart></div>
         </div>
     
     );
